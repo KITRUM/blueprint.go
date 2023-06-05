468b8d7eb7cc9d3c6ca93231df8ed96c91ee8bbf
heartwilltell <heartwilltell@gmail.com>
2023-06-05 17:42:36 +0200

    add terraform config

--- .ops/terraform/main.tf
+++ .ops/terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
     # Google Cloud Platform provider documentation:
     # https://registry.terraform.io/providers/hashicorp/google/latest/docs
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.67.0"
     }
   }
@@ -13,7 +13,7 @@ terraform {
     organization = "heartwilltell"
 
     workspaces {
-      name = "theplace"
+      name = "blueprintgo"
     }
   }
 
@@ -22,4 +22,76 @@ terraform {
 
 provider "google" {
   project = "golang-blueprint"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+variable "project" {
+  type    = string
+  default = "golang-blueprint"
+}
+
+variable "region" {
+  type = string
+  default = "europe-central2"
+}
+
+module "vpc" {
+  source  = "terraform-google-modules/network/google"
+  version = "~> 7.0"
+
+  project_id   = var.project
+  network_name = "${var.project}-vpc"
+  routing_mode = "GLOBAL"
+
+  ip_cidr_range = "10.0.0.0/16"
+
+  subnets = [
+    {
+      subnet_name   = "public-subnet-1"
+      subnet_ip     = "10.0.1.0/24"
+      subnet_region = "${var.region}-a"
+      subnet_type   = "public"
+    },
+    {
+      subnet_name   = "public-subnet-2"
+      subnet_ip     = "10.0.2.0/24"
+      subnet_region = "${var.region}-b"
+      subnet_type   = "public"
+    },
+    {
+      subnet_name   = "public-subnet-3"
+      subnet_ip     = "10.0.3.0/24"
+      subnet_region = "${var.region}-c"
+      subnet_type   = "public"
+    },
+    {
+      subnet_name   = "private-subnet-1"
+      subnet_ip     = "10.0.11.0/24"
+      subnet_region = "${var.region}-a"
+      subnet_type   = "private"
+    },
+    {
+      subnet_name   = "private-subnet-2"
+      subnet_ip     = "10.0.12.0/24"
+      subnet_region = "${var.region}-b"
+      subnet_type   = "private"
+    },
+    {
+      subnet_name   = "private-subnet-3"
+      subnet_ip     = "10.0.13.0/24"
+      subnet_region = "${var.region}-c"
+      subnet_type   = "private"
+    }
+  ]
+
+  routes = [
+    {
+      name             = "allow-egress"
+      description      = "Allow all outgoing traffic"
+      destination_cidr = "0.0.0.0/0"
+      tags             = ["allow-egress"]
+      next_hop_gateway = true
+    }
+  ]
 }
